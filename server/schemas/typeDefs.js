0527ebec7e5c3ee9559ad0d7c1efdbdab43dcf96
Jacobschoenbauer <jhwt65@gmail.com>
2023-02-09 21:13:42 -0600

    Added typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,51 @@
-// const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-// const typeDefs = gql`
-// type 
+const typeDefs = gql`
+  type Record {
+    _id: ID
+    artist: String
+    albumTitle: String
+    label: String
+    catNo: String
+    format: String
+    status: String
+    price: Number
+    dateListed: String
+    comments: String
+    mediaCondition: String
+    sleeveCondition: String
+    quantity: Number
+  }
 
-// module.exports = typeDefs;
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
