3da4e1e13eb32e7901e7298f181b59dde4f16e4f
Nam Bui <nam.bui17@icloud.com>
2023-02-13 19:18:59 -0600

    Generate seed file dataSeed

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -89,7 +89,7 @@ import {
               </Stack>
               <Stack pt={6}>
                 <Text align={'center'}>
-                  Already a user? <Link color={'blue.400'}>Login</Link>
+                  Already a user? <Link to='/login' color={'blue.400'}>Login</Link>
                 </Text>
               </Stack>
             </Stack>
