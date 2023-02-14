4c05b3cd6ef1da21ca071cdda57bdc065a18f352
Jacobschoenbauer <jhwt65@gmail.com>
2023-02-13 21:32:34 -0600

    Start wishlist

--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Image, Box } from '@chakra-ui/react'
+
+
+
+
+
+
 
 function Wishlist() {
 
     return (
-        <div></div>
+        <div>
+<Box boxSize='sm'>
+  <Image src='\assets\images\pexels-elviss-railijs-bitāns-1389429.jpg' alt='Records Cart' />
+  <div className='rtitle'>Record Title</div>
+  <div className='rprice'>Record Price</div>
+</Box>
+
+        </div>
     )
 }
 
