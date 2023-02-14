769ce22900f26a2f653a15e1276fbc7704708f15
Jacobschoenbauer <jhwt65@gmail.com>
2023-02-13 20:58:44 -0600

    finished aboutpage

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,8 +7,13 @@ function About() {
  
   return (
     <div className="background">
-    <div>Hello</div>
-      
+    
+    <section className="about-title">
+        <h2 className="about">About Us</h2>
+        <div className="story">
+          We started selling records because records are our passion. We hope to share our love of records to everyone in the world. This website was created for a quick and simple way for everyone to purchase records in a new and safe way.
+        </div>
+    </section>
         
      
     </div>
