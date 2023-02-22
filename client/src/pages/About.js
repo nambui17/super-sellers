2fae1671aba9f071407f9da34e2d2f8ee296746b
Nam Bui <nam.bui17@icloud.com>
2023-02-21 19:34:36 -0600

    Add wildcard to server

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,21 +1,26 @@
-import React from "react";
-import { Heading, Box, Text } from "@chakra-ui/react";
+import React from 'react';
+import { Heading, Box, Text } from '@chakra-ui/react';
 
-import "../App.css";
-
+import '../App.css';
 
 function About() {
- 
   return (
     <Box className="background" margin={'0 25px 0 25px'} borderRadius={'lg'}>
-    
-    <Box className="about-title">
-        <Heading fontSize='6xl' className="about" marginLeft={'45px'}>About Us</Heading>
-        <Text fontSize='4xl' className="story" textAlign={'left'}>
-          We started selling records because records are our passion. We hope to share our love of records to everyone in the world. This website was created for a quick and simple way for everyone to purchase records in a new and safe way.
+      <Box className="about-title">
+        <Heading
+          fontSize={{ base: '24px', sm: '48px' }}
+          className="about"
+          marginLeft={'45px'}
+        >
+          About Us
+        </Heading>
+        <Text fontSize="4xl" className="story" textAlign={'left'}>
+          We started selling records because records are our passion. We hope to
+          share our love of records to everyone in the world. This website was
+          created for a quick and simple way for everyone to purchase records in
+          a new and safe way.
         </Text>
-    </Box>
-        
+      </Box>
     </Box>
   );
 }
